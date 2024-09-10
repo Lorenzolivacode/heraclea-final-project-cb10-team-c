8222264e6a3aedd870a6e4da38305c091b9405d6
samuelegen <samuelegennaro@gmail.com>
2024-09-11 00:10:40 +0200

    Aggiunta del qr code generator e correzione dei file

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import QrCodeGenerator from "./(components)/Molecoles/QrCodeGenerator";
+
 export default function Home() {
   return (
+    <>
     <div>
       <main>
         <div>Eraclea</div>
         <div>La zona archeo...</div>
+        <QrCodeGenerator/>
       </main>
     </div>
+    </>
   );
 }
