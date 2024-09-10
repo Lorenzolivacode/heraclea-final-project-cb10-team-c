fe998f606308ba3c707895a3f2de311496b1c756
Lorenzolivacode <lorenzo.oliva.1308@gmail.com>
2024-09-10 15:28:58 +0200

    first commit branch lorenzo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <div>
-      <main>ciao, come stai</main>
+      <main>
+        ciao, come stai
+        <h1>Tutto bene, grazie</h1>
+      </main>
     </div>
   );
 }
