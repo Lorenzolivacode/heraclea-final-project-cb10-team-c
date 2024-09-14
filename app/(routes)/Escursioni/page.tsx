345eaabee226ed8e166e02a11e5377da5c7519a6
Lorenzolivacode <lorenzo.oliva.1308@gmail.com>
2024-09-14 19:24:05 +0200

    fix: something

--- app/(routes)/Escursioni/page.tsx
+++ app/(routes)/Escursioni/page.tsx
@@ -1,32 +1,31 @@
-import Card from '@/app/components/Molecoles/Card/Card';
-import styles from '@/app/Home.module.scss';
-import style from './Escursioni.module.scss';
-
+import Card from "@/app/components/Molecoles/Card/Card";
+import styles from "@/app/Home.module.scss";
+import style from "./Escursioni.module.scss";
 
 const arrayTest = [
   {
     name: "In Bici tra Storia e Natura",
     image: "/assets/bici.webp",
     id: "1",
-    roadmap: "/escursioni/1", 
+    roadmap: "/escursioni/1",
   },
   {
     name: "Passeggiata a Cavallo",
     image: "/assets/cavallo.webp",
     id: "2",
-    roadmap: "/escursioni/2", 
+    roadmap: "/escursioni/2",
   },
   {
     name: "Trekking alla Foce",
     image: "/assets/trekking.webp",
     id: "3",
-    roadmap: "/escursioni/3", 
+    roadmap: "/escursioni/3",
   },
 ];
 
 export default function Excursion() {
   return (
-    <main className={styles.main}>
+    <main className="main">
       <h1 className={style.title}>Escursioni</h1>
       {arrayTest.map((point) => (
         <Card
