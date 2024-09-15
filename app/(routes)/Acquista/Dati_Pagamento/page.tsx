dad5a566ef23c07a03c5f9462e7b83ce8fd7790e
Roberta Miosi <148369468+RobertaMi89@users.noreply.github.com>
2024-09-15 18:03:25 +0200

    add function payment modal

--- app/(routes)/Acquista/Dati_Pagamento/page.tsx
+++ app/(routes)/Acquista/Dati_Pagamento/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 import style from "@/app/(routes)/acquista/dati_pagamento/pagamento.module.scss";
 import HeroImage from "@/public/assets/maschera.webp";
 import ApplePay from "@/app/components/Atom/ApplepayBtn/ApplePayBtn";
@@ -11,6 +12,38 @@ import SelectCarta from "@/app/components/Molecoles/SelectCarta/SelectCarta";
 
 function DataPayment() {
   const router = useRouter();
+
+  const [cardNumber, setCardNumber] = useState("");
+  const [selectedCard, setSelectedCard] = useState<string | null>(null);
+  const [cardName, setCardName] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [isFormValid, setIsFormValid] = useState(true);
+
+  const validateForm = () => {
+    return (
+      cardNumber.length === 16 && // numero della carta di 16 cifre
+      selectedCard !== null &&
+      cardName.trim() !== "" &&
+      expiryDate.trim() !== "" &&
+      cvv.trim() !== ""
+    );
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (validateForm()) {
+      router.push("/acquista/acquisto_effettuato");
+    } else {
+      setIsFormValid(false);
+    }
+  };
+
+  useEffect(() => {
+    // Resetta lo stato di validazione quando uno dei campi cambia
+    setIsFormValid(true);
+  }, [cardNumber, selectedCard, cardName, expiryDate, cvv]);
+
   return (
     <>
       <div className={style.main}>
@@ -20,7 +53,7 @@ function DataPayment() {
         {/* FORM*/}
 
         <div className={style.modal}>
-          <form className={style.form}>
+          <form className={style.form} onSubmit={handleSubmit}>
             <div className={style.paymentother}>
               <ApplePay />
               <button name="paypal" type="button">
@@ -40,12 +73,20 @@ function DataPayment() {
                   className={style.input_field}
                   type="text"
                   placeholder="Inserisci il tuo nome completo"
+                  value={cardName}
+                  onChange={(e) => setCardName(e.target.value)}
+                  required
                 />
               </div>
               <div className={style.input_container}>
                 <label className={style.input_label}>Numero della carta</label>
                 <div>
-                  <SelectCarta />
+                  <SelectCarta
+                    cardNumber={cardNumber}
+                    setCardNumber={setCardNumber}
+                    selectedCard={selectedCard}
+                    setSelectedCard={setSelectedCard}
+                  />
                 </div>
               </div>
               <div className={style.input_container}>
@@ -53,15 +94,29 @@ function DataPayment() {
                   Data scadenza carta / CVV
                 </label>
                 <div className={style.split}>
-                  <input className={style.input_field} placeholder="01/23" />
-                  <input className={style.input_field} placeholder="CVV" />
+                  <input
+                    className={style.input_field}
+                    placeholder="01/23"
+                    value={expiryDate}
+                    onChange={(e) => setExpiryDate(e.target.value)}
+                    required
+                  />
+                  <input
+                    className={style.input_field}
+                    placeholder="CVV"
+                    value={cvv}
+                    onChange={(e) => setCvv(e.target.value)}
+                    required
+                  />
                 </div>
               </div>
             </div>
-            <Button
-              text="Avanti"
-              onClick={() => router.push("/acquista/acquisto_effettuato")}
-            />
+            {!isFormValid && (
+              <p className={style.errorMessage}>
+                Per favore, compila tutti i campi.
+              </p>
+            )}
+            <Button text="Avanti" type="submit" />
           </form>
         </div>
       </div>
