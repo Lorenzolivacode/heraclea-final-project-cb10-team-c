0aca2a7a872a543f1d28380bee58a203f3f8adb5
Roberta Miosi <148369468+RobertaMi89@users.noreply.github.com>
2024-09-18 16:24:28 +0200

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
-"use client";
-import { useState } from "react";
 import localFont from "next/font/local";
 import "./globals.css";
-import Footer from "./components/Molecoles/Footer/Footer";
-import Header from "./components/Molecoles/Header/Header";
-import Menu from "./components/Molecoles/Menu/Menu";
+//import ProtectedLayout from "@/app/(protected)/layout";
+import ClientWrapper from "./components/Organism/ClientWrapper/ClientWrapper";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -16,22 +13,18 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
   return (
     <html lang="it">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-        <Menu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-        {children}
-        <Footer setIsMenuOpen={setIsMenuOpen} />
+        <ClientWrapper>{children}</ClientWrapper>
       </body>
     </html>
   );
