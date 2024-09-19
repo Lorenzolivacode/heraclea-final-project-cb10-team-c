67c415d8400c9b6326d0f11fd48e07aa880cdb69
Lejla <lejla.nrk@gmail.com>
2024-09-19 18:11:03 +0200

    style:audioguide modal

--- app/components/Molecoles/Audioguide/AudioGuide.tsx
+++ app/components/Molecoles/Audioguide/AudioGuide.tsx
@@ -12,6 +12,7 @@ interface AudioGuida {
 
 export default function AudioGuide({ label, text, filePath }: AudioGuida) {
   const [isTextVisible, setIsTextVisible] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const [duration, setDuration] = useState<number | null>(null);
   const [currentTime, setCurrentTime] = useState<number>(0);
@@ -68,53 +69,92 @@ export default function AudioGuide({ label, text, filePath }: AudioGuida) {
   };
 
   return (
-    <div className={styles.audioGuideContainer}>
-      <figcaption className={styles.label}>{label}</figcaption>
-      <figure className={styles.audioPlayer}>
-        <audio ref={audioRef} src={filePath} className={styles.audio}></audio>
-        <div className={styles.customControls}>
-          <button className={styles.playPauseButton} onClick={handlePlayPause}>
-            {isPlaying ? (
-              <>
-                <Image
-                  src="/icons/audioguide-icons/pause.svg"
-                  alt="Icon Pause Player"
-                  className={styles.icon}
-                  width={20}
-                  height={20}
-                />
-              </>
-            ) : (
-              <Image
-                src="/icons/audioguide-icons/play.svg"
-                alt="Icon Play Player"
-                className={styles.icon}
-                width={20}
-                height={20}
-              />
-            )}
+    <div>
+      <div className={styles.label}>
+        <figcaption onClick={() => setIsModalOpen(true)}>{label}</figcaption>
+      </div>
+
+      {isModalOpen && (
+        <div
+          className={`${styles.modalOverlay} ${
+            isTextVisible ? styles.active : ""
+          }`}
+        >
+          <button
+            className={styles.closeBtn}
+            onClick={() => setIsModalOpen(false)}
+          >
+            X
           </button>
-          <button>
+          <div className={styles.modalContent}>
             <Image
-              src="/icons/audioguide-icons/stop.svg"
-              width={20}
-              height={20}
-              alt="Icon Stop Player"
-              onClick={handleStopPlaying}
+              src={"/assets/teatro_greco.webp"}
+              alt={"teatro greco di eraclea"}
+              width={70}
+              height={70}
             />
-          </button>
-        </div>
+            <figure className={styles.audioPlayer}>
+              <audio
+                ref={audioRef}
+                src={filePath}
+                className={styles.audio}
+              ></audio>
+              <div className={styles.customControls}>
+                <button
+                  className={styles.playPauseButton}
+                  onClick={handlePlayPause}
+                >
+                  {isPlaying ? (
+                    <Image
+                      src="/icons/audioguide-icons/pause.svg"
+                      alt="Icon Pause Player"
+                      className={styles.icon}
+                      width={20}
+                      height={20}
+                    />
+                  ) : (
+                    <Image
+                      src="/icons/audioguide-icons/play.svg"
+                      alt="Icon Play Player"
+                      className={styles.icon}
+                      width={20}
+                      height={20}
+                    />
+                  )}
+                </button>
+                <button>
+                  <Image
+                    src="/icons/audioguide-icons/stop.svg"
+                    width={20}
+                    height={20}
+                    alt="Icon Stop Player"
+                    onClick={handleStopPlaying}
+                  />
+                </button>
+              </div>
+              <div className={styles.timeDisplay}>
+                <span>{formatTime(currentTime)}</span>/
+                <span>{formatTime(duration)}</span>
+              </div>
+              <button
+                className={styles.textToggleButton}
+                onClick={toggleVisibility}
+              >
+                {isTextVisible ? "Hide Text" : "Show Text"}
+              </button>
+            </figure>
+            <p
+              className={`${styles.textContent} ${
+                isTextVisible ? styles.textVisible : ""
+              }`}
+            >
+              {text}
+            </p>
 
-        <div className={styles.timeDisplay}>
-          <span>{formatTime(currentTime)}</span>/
-          <span>{formatTime(duration)}</span>
+            {/* {isTextVisible && <p className={styles.textContent}>{text}</p>} */}
+          </div>
         </div>
-        <button className={styles.textToggleButton} onClick={toggleVisibility}>
-          {isTextVisible ? "Hide Text" : "Show Text"}
-        </button>
-      </figure>
-
-      {isTextVisible && <p className={styles.textContent}>{text}</p>}
+      )}
     </div>
   );
 }
